5331b7d3b0cc4b5f181e3f8c40f7450b5a2fa46b
Lluvia-18 <1225100383.levl@gmail.com>
2026-09-24 16:02:14 -0600

    Mi primer metodo de recursividad

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,18 +2,20 @@ package datos.unidad1.recursividad;
 
 public class Recursividad {
 
-	public static void saludo(int total) {
+	public static void saludo(String nombre, int total) {
 	
 		if(total <= 0)
 			return;
 		else{
-			System.out.println("Hola");
-			saludo(total-1);
+			System.out.println("Hola"+ nombre);
+			saludo(nombre,total-1);
 		}
 	}
 
 	public static void main(String[] args) {
-		saludo(10);
+                      saludo("Lluvia", 10);
+
 	
 	}
 }
+
